d433b33c5e474bfdc00510c7bd0ef4e8578bf730
JaredMindel <102982231+JaredMindel@users.noreply.github.com>
2022-04-06 08:29:23 -0700

    Add files via upload

---
+++ Guessing Game.py
@@ -0,0 +1,13 @@
+### Guessing Game ###
+
+guess = "Thanos"
+
+def guessing_game(guess):
+  if len(guess) = 1:
+    guess_type = "letter"
+  else: 
+    guess_type = "word"
+  if guess_type == "letter"
+  
+  
+###if they have more than 2 guesses left#
